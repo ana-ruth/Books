ab577df02d58204bf2e667d37c9453c2a9540116
ana-ruth <114896716+ana-ruth@users.noreply.github.com>
2024-08-06 15:42:50 -0400

    Initial commit

---
+++ script.js
@@ -0,0 +1,19 @@
+const myLibrary = [];
+
+
+function Book(title,author,pages,read)
+{
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+
+    this.info = function()
+    {
+        return ""+title+" by "+author+", "+pages+", "+read;
+    };
+}
+
+function addBookToLibrary() {
+    // do stuff here
+  }
