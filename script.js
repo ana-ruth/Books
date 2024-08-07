bd54976d298885f8ae15a0f6dfecbb630c470b59
ana-ruth <114896716+ana-ruth@users.noreply.github.com>
2024-08-07 17:48:50 -0400

    card initial interface

--- script.js
+++ script.js
@@ -14,6 +14,74 @@ function Book(title,author,pages,read)
     };
 }
 
-function addBookToLibrary() {
-    // do stuff here
-  }
+function addBookToLibrary(Book) {
+    myLibrary.push(Book);
+}
+
+const library = document.querySelector("#library");
+
+function displayBooks()
+{
+    for (let i=0; i<myLibrary.length;i++)
+    {
+
+        const card = document.createElement("div");
+        card.classList.add("card");
+     
+        const libraryCard = document.createElement("h1");
+        libraryCard.textContent = "LIBRARY CARD";
+        libraryCard.classList.add("libraryCard");
+        card.appendChild(libraryCard);
+
+        const bookTitle = document.createElement("h2");
+        bookTitle.classList.add("bookTitle");
+        bookTitle.textContent = "Title: " + myLibrary[i].title;
+        card.appendChild(bookTitle);
+
+        const bookAuthor = document.createElement("h2");
+        bookAuthor.classList.add("bookAuthor");
+        bookAuthor.textContent = "Author: " + myLibrary[i].author;
+        card.appendChild(bookAuthor);
+
+
+        const headings = document.createElement("div");
+        headings.classList.add("headings");
+
+        const pagesHeading = document.createElement("h3");
+        pagesHeading.textContent = "Pages";
+        headings.appendChild(pagesHeading);  
+        
+        const line = document.createElement("div");
+        headings.appendChild(line);    
+
+        const readHeading = document.createElement("h3");
+        readHeading.textContent = "Read?";
+        headings.appendChild(readHeading); 
+
+        card.appendChild(headings);
+
+
+        const bookPages = document.createElement("h3");
+        bookPages.classList.add("bookPages");
+        bookPages.textContent = myLibrary[i].pages;
+        card.appendChild(bookPages);
+
+        const hasReadBook = document.createElement("h4");
+        hasReadBook.classList.add("hasReadBook");
+        hasReadBook.textContent = myLibrary[i].read;
+        card.appendChild(hasReadBook);
+
+        library.appendChild(card);
+    }
+}
+
+const firstBook = new Book("Jane Eyre","Charlotte Bronte","189 pages","Yes")
+const secondBook = new Book("Emma","Jane Austen","215 pages","Yes")
+
+
+myLibrary.push(firstBook)
+myLibrary.push(secondBook);
+
+
+/*addBookToLibrary();*/
+displayBooks();
