69b67a0254f3b33b3389fa4eec737ca692eb073b
ana-ruth <114896716+ana-ruth@users.noreply.github.com>
2024-08-13 17:43:03 -0400

    Insert new books button and form

--- script.js
+++ script.js
@@ -14,15 +14,29 @@ function Book(title,author,pages,read)
     };
 }
 
+
+function createBook(inputBook){
+    const values = inputBook.split(",");
+    const title = values[0];
+    const author = values[1];
+    const pages = values[2];
+    const read = values[3]
+
+    const newBook = new Book(title,author,pages,read)
+    addBookToLibrary(newBook);
+}
+
+
 function addBookToLibrary(Book) {
     myLibrary.push(Book);
 }
 
 const library = document.querySelector("#library");
+let deleteBook;
 
-function displayBooks()
+function displayBooks(initialPosition,length)
 {
-    for (let i=0; i<myLibrary.length;i++)
+    for (let i=initialPosition; i<length;i++)
     {
 
         const card = document.createElement("div");
@@ -76,17 +90,58 @@ function displayBooks()
 
         card.appendChild(pgsRead);
 
+        deleteBook = document.createElement("button");
+        deleteBook.textContent = "REMOVE";
+        card.appendChild(deleteBook);
+
         library.appendChild(card);
     }
 }
 
-const firstBook = new Book("Jane Eyre","Charlotte Bronte","189 pgs","Yes")
-const secondBook = new Book("Emma","Jane Austen","215 pgs","Yes")
+const firstBook = new Book("Jane Eyre","Charlotte Bronte","189","Yes")
+const secondBook = new Book("Emma","Jane Austen","215","Yes")
 
 
 myLibrary.push(firstBook)
 myLibrary.push(secondBook);
 
 
-/*addBookToLibrary();*/
-displayBooks();
+displayBooks(0,myLibrary.length); //initial display of books
+
+
+/*Get new Book's details from the form */
+const showButton = document.getElementById("showDialog");
+const favDialog = document.getElementById("favDialog");
+const outputBox = document.querySelector("output");
+const confirmBtn = favDialog.querySelector("#confirmBtn");
+
+const userTitle = document.querySelector("#Book")
+const userAuthor = document.querySelector("#Author")
+const userPages = document.querySelector("#Pages")
+const userRead = document.querySelector("#Read")
+
+let newBook;
+
+// "Show the dialog" button opens the <dialog> modally
+showButton.addEventListener("click", () => {
+  favDialog.showModal();
+});
+
+// "Cancel" button closes the dialog without submitting because of [formmethod="dialog"], triggering a close event.
+favDialog.addEventListener("close", (e) => {
+  newBook = favDialog.returnValue;
+  createBook(newBook);
+  displayBooks(myLibrary.length-1,myLibrary.length); //display the new book only
+});
+
+// Prevent the "confirm" button from the default behavior of submitting the form, and close the dialog with the `close()` method, which triggers the "close" event.
+confirmBtn.addEventListener("click", (event) => {
+  event.preventDefault(); //prevent from submitting form
+  favDialog.close(userTitle.value+","+userAuthor.value+","+userPages.value+","+userRead.value);
+});
+
+/*
+deleteBook.addEventListener('click',function(){
+    delete myArray[]; 
+});
+*/
